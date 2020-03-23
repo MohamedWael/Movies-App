55912624f1bcf81e981b53be3ae1434fd89938e8
Mohamed Wael <imohamedwael@outlook.com>
2020-03-23 23:36:31 +0200

    adding service locator and constants file

---
+++ app/src/main/java/com/github/mohamedwael/movieapp/applevel/Constants.kt
@@ -0,0 +1,9 @@
+package com.github.mohamedwael.movieapp.applevel
+
+const val BASE_API = "https://private-d502e-capiterapis.apiary-mock.com/"
+const val MOVIES = "movies"
+const val MOVIE_ID = "movie_id"
+
+
+
+
---
+++ app/src/main/java/com/github/mohamedwael/movieapp/applevel/ServiceLocator.kt
@@ -0,0 +1,16 @@
+package com.github.mohamedwael.movieapp.applevel
+
+/**
+ * This service locator wil act as a replacment for the dependency injection for simplicity
+ */
+object ServiceLocator {
+    var serviceMap: MutableMap<String, Any> = mutableMapOf()
+
+    fun addService(key: String, value: Any) {
+        serviceMap[key] = value
+    }
+
+    fun retrieveService(key: String) =
+        checkNotNull(serviceMap[key]) { "Service associated with the key: $key is not found!" }
+
+}
